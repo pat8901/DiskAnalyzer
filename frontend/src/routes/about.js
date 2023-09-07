57f0c7e91377a0d59c39d2b3bdf0c4e373cbd107
Patrick <seitzpr@gmail.com>
2023-09-07 12:52:47 -0500

    Working on buttons

--- frontend/src/routes/about.js
+++ frontend/src/routes/about.js
@@ -2,27 +2,21 @@ import React from 'react'
 import { TextField } from '@mui/material'
 import { Link } from 'react-router-dom'
 import Logo from '../logos/Notre_Dame_Fighting_Irish_logo.png'
+import Logo2 from '../logos/logo512.png'
 
-function About () {
+function About() {
   return (
     <div className='contain'>
       <div className='item-box'>
         <div className='box1'>
-          <div className='logo-box'>
-            <img className='logo' src={Logo} alt='' />
+          <div style={{ backgroundColor: "white" }} className='logo-box'>
+            <img style={{ width: "18vh" }} className='logo' src={Logo2} alt='' />
           </div>
           <div className='route-box'>
-            <div className='route'>
-              <Link className='route' to='/'>
-                Home
-              </Link>
-            </div>
-            <div className='route'>
-              <Link className='route' to='/test'>
-                Test
-              </Link>
-            </div>
-            <div className='route'>route 3</div>
+            <ul className='list'>
+              <a href='/'>Home</a>
+              <a href='/test'>User Search</a>
+            </ul>
           </div>
         </div>
         <div className='box2'>
@@ -36,7 +30,7 @@ function About () {
             /> */}
             About Page
           </div>
-          <div className='item1'>
+          <div style={{ backgroundColor: "white" }} className='item1'>
             <div className='footer'></div>
           </div>
         </div>
