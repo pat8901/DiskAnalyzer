a1fc01eb71b0f4d933005de7add97172e274ce7b
Patrick <seitzpr@gmail.com>
2023-10-04 13:27:45 -0500

    added upload page

--- frontend/src/routes/App.js
+++ frontend/src/routes/App.js
@@ -4,16 +4,18 @@ import Home from './home'
 import Group from './group'
 import About from './about'
 import Search from './search'
+import Upload from './upload'
 
 import User from './user'
 
-function App () {
+function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='group' element={<Group />} />
         <Route path='about' element={<About />} />
+        <Route path='upload' element={<Upload />} />
         <Route path='search' element={<Search />} />
         <Route path='search/:slug' element={<User />} />
       </Routes>
