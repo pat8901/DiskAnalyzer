7715cff7ea7126549ccff5ff25bdde060876b215
Patrick <seitzpr@gmail.com>
2023-10-09 07:58:07 -0500

    Started to work on file rejection system

--- frontend/src/components/FileUpload.js
+++ frontend/src/components/FileUpload.js
@@ -6,8 +6,11 @@ const FileUpload = () => {
   const handleFileUpload = event => { // Why and what is "event"?
     const file = event.target.files[0] // Why and what is target.files[0] ?
     console.log("file:")
+    console.log(file.name) // Could check on the frontend to make sure file is proper name format. cancel request if it doesnt follow the form of "Storage_Rep_YEAR-MONTH-DAY.pdf"
     console.log(file)
 
+    // The code below will only run if the file has the proper name
+    // The next step would be to check to make sure the contents of the file upload is correct. How would I go about checking for this on the backend/frontend
     // Creates an object that will take key:value pairs. In this case files
     const formData = new FormData()
     formData.append('file', file) // Inserts the key:value pairs in the object
