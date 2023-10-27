3be7fcd805360a580ebe79641d1f808d9c76edef
Patrick <seitzpr@gmail.com>
2023-10-27 12:01:34 -0400

    Round 1: Refactored tools.py

--- backend/plots/tools.py
+++ backend/plots/tools.py
@@ -1,10 +1,11 @@
-import sys
-from pypdf import PdfReader
-import csv
 import pandas as pd
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+
+# import sys
+# import csv
+# import matplotlib
+# from pypdf import PdfReader
 
 
 # +======================================================================================+
@@ -12,12 +13,13 @@ import numpy as np
 # |             Uses the total storage of a user to calculate the divisor                |
 # +======================================================================================+
 def getDivisor(input):
-    # may be faster to do bit shifting?
+    # may be faster to do bit shifting or some other way?
     terabyte = 1000000000
     gigabyte = 1000000
     megabyte = 1000
     kilobyte = 1
 
+    # Using the users total storage, determining where it lies on the scale
     if terabyte <= input:
         return terabyte
     elif gigabyte <= input < terabyte:
@@ -29,33 +31,7 @@ def getDivisor(input):
 
 
 # +======================================================================================+
-# |       Tool to determine the proper divisor to use when given a pandas dataframe      |
-# |             Uses the total storage of a user to calculate the divisor                |
-# +======================================================================================+
-def getDivisorArray(inputArray):
-    # may be faster to do bit shifting?
-    terabyte = 1000000000
-    gigabyte = 1000000
-    megabyte = 1000
-    kilobyte = 1
-    outputArray = np.zeros(len(inputArray))
-
-    np.array(inputArray)
-    for i in range(inputArray):
-        if terabyte <= input:
-            outputArray = np.insert(outputArray, i, terabyte)
-        elif gigabyte <= input < terabyte:
-            outputArray = np.insert(outputArray, i, gigabyte)
-        elif megabyte <= input < gigabyte:
-            outputArray = np.insert(outputArray, i, megabyte)
-        else:
-            outputArray = np.insert(outputArray, i, kilobyte)
-
-    return outputArray
-
-
-# +======================================================================================+
-# |               Tool to number to its corresponding name returns a string              |
+# |  Tool that when given a number will gives its corresponding name in character form   |
 # +======================================================================================+
 def getChartCounter(input):
     terabyte = 1000000000
@@ -74,9 +50,11 @@ def getChartCounter(input):
 
 
 # +======================================================================================+
-# |               Tool to number to its corresponding name returns a string              |
+# |                Tool that when given a name will gives its corresponding              |
+# |                         name in abbreviated character form                           |
 # +======================================================================================+
 def getUnit(input):
+    # Getting the name string and returning abbreviated name
     if input == "Terabytes":
         return "TB"
     elif input == "Gigabytes":
@@ -85,61 +63,6 @@ def getUnit(input):
         return "MB"
     else:
         return "KB"
-
-
-# +======================================================================================+
-# |             This function sums the column of Total Storage and prints                |
-# |                           onto the screen in terabytes                               |
-# +======================================================================================+
-def getTotalStorage():
-    total = 0
-    df = pd.read_csv("csv/research.csv")
-    total = df["Tot.Used Space"].sum()
-    terabyte = total / 1000000000
-    print(f"Total Storage (KB): {total}")
-    print(f"Total Storage (TB): {terabyte}")
-
-
-# +======================================================================================+
-# |             This fucntion may be helpful in adding labels to graphs                  |
-# +======================================================================================+
-def addlabels(x, y):
-    for i in range(len(x)):
-        plt.text(i, y[i], y[i])
-
-
-# +======================================================================================+
-# |             Creates a list of bin labels to be used with frequency graphs            |
-# +======================================================================================+
-def binLabelCreator(amount, step, start):
-    bin_labels = []
-    bin_labels.append(f"{start}-{step}")
-    left = step
-    for i in range(amount - 1):
-        bin_labels.append(f"{left}-{left+step}")
-        left = left + step
-    return bin_labels
-
-
-# +======================================================================================+
-# |             Creates a list of bin to be used with frequency graphs                   |
-# +======================================================================================+
-def binCreator(amount, step, start):
-    bins = []
-    bins.append(start)
-    left = step
-    for i in range(amount):
-        bins.append(left)
-        left = left + step
-    # bins.append()
-    return bins
-
-
-# +======================================================================================+
-# |                       Reformates the date to American Standard                       |
-# +======================================================================================+
-def dateFormatter(date):
-    print(date)
 
 
 # +======================================================================================+
@@ -164,7 +87,103 @@ def getFileName(report_file):
 
 
 # +======================================================================================+
+# |                       Reformates the date to American Standard                       |
+# |                              *Feature request*                                       |
+# +======================================================================================+
+def dateFormatter(date):
+    print(date)
+
+
+# +======================================================================================+
 # |                Tool to choose what format you want to save figures in                |
+# |                              *Feature request*                                       |
 # +======================================================================================+
 def getFormat():
     print(f"you selected png")
+
+
+# +======================================================================================+
+# |             *Not Used I believe / Hard coded for research / May delete*              |
+# |             This function sums the column of Total Storage and prints                |
+# |                           onto the screen in terabytes                               |
+# |             *Not Used I believe / Hard coded for research / May delete*              |
+# +======================================================================================+
+def getTotalStorage():
+    # Reading the csv into a pandas dataframe
+    df = pd.read_csv("csv/research.csv")
+    # Getting the total by summing the total used space column
+    total = df["Tot.Used Space"].sum()
+    # Converting the units to terabytes
+    terabyte = total / 1000000000
+    # Displaying info
+    print(f"Total Storage (KB): {total}")
+    print(f"Total Storage (TB): {terabyte}")
+
+
+# +======================================================================================+
+# |                     *Not Used I believe / May delete*                                |
+# |             This fucntion may be helpful in adding labels to graphs                  |
+# |                     *Not Used I believe / May delete*                                |
+# +======================================================================================+
+def addlabels(x, y):
+    for i in range(len(x)):
+        plt.text(i, y[i], y[i])
+
+
+# +======================================================================================+
+# |                            *This is not really used*                                 |
+# |             Creates a list of bin labels to be used with frequency graphs            |
+# +======================================================================================+
+def binLabelCreator(amount, step, start):
+    bin_labels = []
+    # Append starting point
+    bin_labels.append(f"{start}-{step}")
+    left = step  # Set left side of the range to the step i.e. right side
+    for i in range(amount - 1):  # Loop though amount
+        # Append the left side and right side to create a range label
+        bin_labels.append(f"{left}-{left+step}")
+        left = left + step  # Set the left to the right
+    return bin_labels
+
+
+# +======================================================================================+
+# |                            *This is not really used*                                 |
+# |             Creates a list of bin to be used with frequency graphs                   |
+# +======================================================================================+
+def binCreator(amount, step, start):
+    bins = []
+    bins.append(start)  # Append the first bin
+    left = step
+    # Loop through the range of amount
+    for i in range(amount):
+        bins.append(left)  # Append left
+        left = left + step  # Set left equal to step
+    return bins
+
+
+# +======================================================================================+
+# |             *Not Used / Testing function / Will proabably delete soon*               |
+# |       Tool to determine the proper divisor to use when given a pandas dataframe      |
+# |             Uses the total storage of a user to calculate the divisor                |
+# |             *Not Used / Testing function / Will proabably delete soon*               |
+# +======================================================================================+
+def getDivisorArray(inputArray):
+    # may be faster to do bit shifting?
+    terabyte = 1000000000
+    gigabyte = 1000000
+    megabyte = 1000
+    kilobyte = 1
+    outputArray = np.zeros(len(inputArray))
+
+    np.array(inputArray)
+    for i in range(inputArray):
+        if terabyte <= input:
+            outputArray = np.insert(outputArray, i, terabyte)
+        elif gigabyte <= input < terabyte:
+            outputArray = np.insert(outputArray, i, gigabyte)
+        elif megabyte <= input < gigabyte:
+            outputArray = np.insert(outputArray, i, megabyte)
+        else:
+            outputArray = np.insert(outputArray, i, kilobyte)
+
+    return outputArray
